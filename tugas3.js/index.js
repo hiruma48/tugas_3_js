d79a4bc7f810a549c7127513c1fc31affc7aa967
tri ariyanto <triyanto129@gmail.com>
2023-05-01 22:25:55 +0700

    tugas 3 js
    mengumpulkan

---
+++ tugas3.js/index.js
@@ -0,0 +1,9 @@
+function minta(){
+    var buang = "ditolak";
+return buang;
+}
+var ambil = minta()
+
+console.log(minta());
+console.log(minta(ambil));
+
---
+++ tugas3.js/parameter.js
@@ -0,0 +1,8 @@
+function data(a,b,c,d){
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+}
+data("aku" , "suka" , "kamu" , "shani");
+data()
